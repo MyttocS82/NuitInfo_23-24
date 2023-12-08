6083c5a3c7f0fde7b0331dab3f4b8c0da2f2722e
MyttocS82 <153259172+MyttocS82@users.noreply.github.com>
2023-12-08 07:19:12 +0100

    Add files via upload

---
+++ script/scrip.js
@@ -0,0 +1,11 @@
+function themeVert() {
+  document.documentElement.style.setProperty('--vertClassique', '#BDDDC9')
+  document.documentElement.style.setProperty('--vertHeader', '#DDEEE2')
+  document.documentElement.style.setProperty('--bleue', '#6E93D6')
+}
+
+function themeBlue() {
+  document.documentElement.style.setProperty('--vertClassique', '#95B6E4')
+  document.documentElement.style.setProperty('--vertHeader', '#E5F1FA')
+  document.documentElement.style.setProperty('--bleue', '#BDDDC9')
+}
